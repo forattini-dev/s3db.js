61907db33d277900c55d8cd7682e9c0383734c12
Filipe Forattini <filipe@forattini.com.br>
2022-11-16 15:12:16 -0300

    docs: example and table of content

--- examples/read-stream-to-csv.js
+++ examples/read-stream-to-csv.js
@@ -1,6 +1,8 @@
 require("dotenv").config({ path: `${__dirname}/../.env` });
 
-const Multiprogress = require("multi-progress");
+const fs = require("fs");
+const ProgressBar = require("progress");
+const { Transform } = require("stream");
 
 const { S3db } = require("../build");
 
@@ -20,35 +22,44 @@ async function main() {
   await client.connect();
   const total = await client.resource("leads").count();
 
-  console.log(`reading ${total} leads.`);
   console.log(`parallelism of ${PARALLELISM} requests.\n`);
-  const multi = new Multiprogress(process.stdout);
-
-  const options = {
-    total,
-    width: 30,
-    incomplete: " ",
-  };
-
-  const barIds = multi.newBar(
-    "reading-ids   :current/:total (:percent)  [:bar]  :rate/bps  :etas (:elapseds)",
-    options
-  );
-
-  const barData = multi.newBar(
+  const barData = new ProgressBar(
     "reading-data  :current/:total (:percent)  [:bar]  :rate/bps  :etas (:elapseds)",
-    options
+    {
+      total,
+      width: 30,
+      incomplete: " ",
+    }
   );
+
+  const filename = __dirname + "/tmp/leads." + Date.now() + ".csv";
 
   const stream = await client.resource("leads").stream();
+  const streamWrite = fs.createWriteStream(filename);
 
-  console.time("reading");
-  stream.on("id", () => barIds.tick());
+  const transformer = new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      this.push(
+        [chunk.id, chunk.email, chunk.name, chunk.token].join(";") + "\n"
+      );
+      callback();
+    },
+  });
+
+  console.time("reading-data");
+
   stream.on("data", () => barData.tick());
   stream.on("end", () => {
-    console.timeEnd("reading");
+    console.timeEnd("reading-data");
+    const { size } = fs.statSync(filename);
+    console.log(
+      `\nresource leads total size: ${(size / (1024 * 1000)).toFixed(2)} Mb`
+    );
     process.stdout.write("\n\n");
   });
+
+  stream.pipe(transformer).pipe(streamWrite);
 }
 
 main();
---
+++ examples/read-stream-to-zip.js
@@ -0,0 +1,64 @@
+require("dotenv").config({ path: `${__dirname}/../.env` });
+
+const fs = require("fs");
+const zlib = require("node:zlib");
+const ProgressBar = require("progress");
+const { Transform } = require("node:stream");
+const { pipeline } = require("node:stream/promises");
+
+const { S3db } = require("../build");
+
+const { bucket, accessKeyId, secretAccessKey } = process.env;
+
+const PARALLELISM = 100;
+const CONNECTION_STRING =
+  `s3://${accessKeyId}:${secretAccessKey}@${bucket}/databases/examples-` +
+  new Date().toISOString().substring(0, 10);
+
+async function main() {
+  const client = new S3db({
+    uri: CONNECTION_STRING,
+    parallelism: PARALLELISM,
+  });
+
+  await client.connect();
+  const total = await client.resource("leads").count();
+
+  console.log(`parallelism of ${PARALLELISM} requests.\n`);
+  const barData = new ProgressBar(
+    "reading-data  :current/:total (:percent)  [:bar]  :rate/bps  :etas (:elapseds)",
+    {
+      total,
+      width: 30,
+      incomplete: " ",
+    }
+  );
+
+  const filename = __dirname + "/tmp/leads." + Date.now() + ".csv.gzip";
+  const stream = await client.resource("leads").stream();
+  const streamWrite = fs.createWriteStream(filename);
+
+  const transformer = new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      this.push([chunk.id, chunk.name, chunk.token].join(";") + "\n");
+      callback();
+    },
+  });
+
+  console.time("reading-data");
+
+  stream.on("data", () => barData.tick());
+  stream.on("end", () => {
+    console.timeEnd("reading-data");
+    const { size } = fs.statSync(filename);
+    console.log(
+      `\nresource leads zip size: ${(size / (1024 * 1000)).toFixed(2)} Mb`
+    );
+    process.stdout.write("\n\n");
+  });
+
+  pipeline(stream, transformer, zlib.createGzip(), streamWrite);
+}
+
+main();
