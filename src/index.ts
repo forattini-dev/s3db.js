2cf97f711d2c7bcacdbf2911cdbeeb37f077835d
Filipe Forattini <filipe@forattini.com.br>
2022-12-15 08:53:42 -0300

    refactor: now package exports main classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { S3Database, S3db } from './s3-database.class'
 export { S3Cache } from './cache/s3-cache.class'
 export { S3Client } from './s3-client.class'
+
 export * from './plugins'
+export * from './stream'
