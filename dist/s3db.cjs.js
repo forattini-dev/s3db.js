e8446cc44ed5ac8203239b7c98cd43a7121e0c1d
Filipe Forattini <filipe@forattini.com.br>
2025-06-27 17:16:17 -0300

    chore: deployment scripts

--- dist/s3db.cjs.js
+++ dist/s3db.cjs.js
@@ -9701,7 +9701,7 @@ class Database extends EventEmitter {
     this.version = "1";
     this.s3dbVersion = (() => {
       try {
-        return true ? "4.0.0" : "latest";
+        return true ? "4.0.1" : "latest";
       } catch (e) {
         return "latest";
       }
