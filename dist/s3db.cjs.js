dd2df8796bc9dc358e8292f44dbad65a429c9dca
Filipe Forattini <filipe@vitta.me>
2025-08-14 15:19:52 -0300

    chore: new version built

--- dist/s3db.cjs.js
+++ dist/s3db.cjs.js
@@ -13522,7 +13522,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = try_fn_default(() => true ? "8.1.3" : "latest");
+      const [ok, err, version] = try_fn_default(() => true ? "8.2.0" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
