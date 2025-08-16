421b5c1c10077f6c8561cc3248a4234638847a7c
Filipe Forattini <filipe@vitta.me>
2025-08-16 17:09:14 -0300

    chore: package version

--- dist/s3db.cjs.js
+++ dist/s3db.cjs.js
@@ -9046,7 +9046,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "9.1.0" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "9.2.0" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
