e48db0493f9d489428226238758d6a5415db313c
Filipe Forattini <filipe@forattini.com.br>
2025-08-24 11:01:18 -0300

    fix: partitions behavios after resource updates

--- dist/s3db.es.js
+++ dist/s3db.es.js
@@ -9696,7 +9696,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "9.2.2" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "9.3.0" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
