359f39f72c0d4805faf6aac1bba54fbc311f3322
Filipe Forattini <filipe@forattini.com.br>
2025-08-31 01:28:45 -0300

    chore: built assets

--- dist/s3db.es.js
+++ dist/s3db.es.js
@@ -10153,7 +10153,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "9.3.0" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "10.0.0" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
