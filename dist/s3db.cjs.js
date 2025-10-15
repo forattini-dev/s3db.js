88ddbf63171f20a3076a1f1d02b426f07b5bc642
Filipe Forattini <filipe@forattini.com.br>
2025-10-15 15:49:35 -0300

    11.1.1

--- dist/s3db.cjs.js
+++ dist/s3db.cjs.js
@@ -13751,7 +13751,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "11.1.0" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "11.1.1" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
