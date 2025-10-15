29c94908f0ae87444377e07c7f0ba92f9372d74e
Filipe Forattini <filipe@forattini.com.br>
2025-10-15 17:06:38 -0300

    chore: build version

--- dist/s3db.es.js
+++ dist/s3db.es.js
@@ -13747,7 +13747,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "11.1.2" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "11.1.3" : "latest");
       return ok ? version : "latest";
     })();
     this.resources = {};
