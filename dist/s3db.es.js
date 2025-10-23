4a2d736cc4b43706337f5e8e469e8d7cc13df7c9
Filipe Forattini <filipe.forattini@stone.com.br>
2025-10-23 16:38:01 -0300

    12.2.0

--- dist/s3db.es.js
+++ dist/s3db.es.js
@@ -22391,7 +22391,7 @@ class Database extends EventEmitter {
     this.id = idGenerator(7);
     this.version = "1";
     this.s3dbVersion = (() => {
-      const [ok, err, version] = tryFn(() => true ? "12.1.0" : "latest");
+      const [ok, err, version] = tryFn(() => true ? "12.2.0" : "latest");
       return ok ? version : "latest";
     })();
     this._resourcesMap = {};
